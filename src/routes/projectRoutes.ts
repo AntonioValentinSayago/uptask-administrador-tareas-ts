782a40d183ce163f2c4fa1fd13c4f43aa67e01a9
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-03-03 21:23:00 -0600

    Creacion de middleare con exprees validator para los campos del body

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middleware/validations";
 
 const router = Router();
 
-router.post('/', ProjectController.createProjects);
+router.post('/', 
+    body('projectName')
+        .notEmpty().withMessage('El nombre del proyecto es requerido'),
+    body('clientName')
+        .notEmpty().withMessage('El nombre del cliente es requerido'),
+    body('description')
+        .notEmpty().withMessage('La descripción del proyecto es requerida'),
+    handleInputErrors,
+    ProjectController.createProjects),
 router.get('/', ProjectController.getAllProjects);
 
 export default router;
