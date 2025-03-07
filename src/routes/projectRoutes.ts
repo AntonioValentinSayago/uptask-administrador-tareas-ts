aff4cf116a31278797ee733909d9a6cd41a2f2b4
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-03-06 22:10:38 -0600

    Actualizacion de las rutas para agregar tareas

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { body, param } from "express-validator";
 import { ProjectController } from "../controllers/ProjectController";
 import { handleInputErrors } from "../middleware/validations";
+import { TaskController } from "../controllers/TaskController";
 
 const router = Router();
 
@@ -38,6 +39,10 @@ router.put('/:id',
 router.delete('/:id',
     param('id').isMongoId().withMessage('El id del proyecto no es válido'),
     handleInputErrors,
-    ProjectController.deleteProject);
+    ProjectController.deleteProject
+);
+
+//** Routes for tasks */
+router.post('/:projectId/tasks', TaskController.createTask)
 
 export default router;
