61e5ff94ab1cf68fb48c86be6670a63171219d98
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-03-11 20:56:59 -0600

    Se crea la funcion getProjectsTaks para obtener todas las tareas

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,27 +1,25 @@
 import type { Request, Response } from "express";
-import Project from "../models/Project";
 import Task from "../models/Task";
 
 export class TaskController {
-    static async createTask(req: Request, res: Response) : Promise<void> {
-        
-        const { projectId } = req.params;
-        const project = await Project.findById(projectId);
-        if (!project) {
-            const error = new Error('Proyecto no encontrado');
-            res.status(404).json({error: error.message});
-            return;
-        }
+    static async createTask(req: Request, res: Response) {
+
         try {
             const task = new Task(req.body);
-            task.project = project.id;
-            project.tasks.push(task);
-            await task.save();
-            await project.save();
+            task.project = req.project.id;
+            req.project.tasks.push(task.id);
+            await Promise.allSettled([task.save(), req.project.save()]);
             res.send('Tarea Creada Correctamente');
         } catch (error) {
-            console.log(error);
-            res.status(500).send('Internal server error');
+            res.status(500).json({error: 'Hubo un error'})
+        }
+    }
+    static getProjectsTasks = async (req: Request, res: Response) => {
+        try {
+            const tasks = await Task.find({ project: req.project.id }).populate('project');
+            res.json(tasks);
+        } catch (error) {
+            res.status(500).send({error: 'Hubo un error'});
         }
     }
 }
