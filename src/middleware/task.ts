477be5099b775845261a34944ad5dd3df2f8653c
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-03-21 22:44:15 -0600

    Nuevo middleware para la validacion de tareas existentes

---
+++ src/middleware/task.ts
@@ -0,0 +1,26 @@
+import type { Request, Response, NextFunction } from 'express';
+import Project, { ITask } from '../models/Task';
+
+declare global {
+    namespace Express {
+        interface Request {
+            task: ITask;
+        }
+    }
+}
+
+export async function taskExists(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const { taskId } = req.params;
+        const task = await Project.findById(taskId);
+        if (!task) {
+            const error = new Error('Tarea no enontrada');
+            res.status(404).json({ error: error.message });
+            return;
+        }
+        req.task = task;
+        next();
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
