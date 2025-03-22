e5e0e47b8efa51edf44386799abfa86f33e63ccb
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-03-21 22:44:44 -0600

    Act del nombre de la funcion del middleware

--- src/middleware/project.ts
+++ src/middleware/project.ts
@@ -9,7 +9,7 @@ declare global {
     }
 }
 
-export async function validateProjectExists(req: Request, res: Response, next: NextFunction): Promise<void> {
+export async function projectExists(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
         const { projectId } = req.params;
         const project = await Project.findById(projectId);
