d94d6dcc5dc483c04f740fe827558c3a56352c69
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-03-31 20:29:33 -0600

    Actualizacion y creacion de la funcion getProjects

--- uptask-frontend/src/api/ProjectApi.ts
+++ uptask-frontend/src/api/ProjectApi.ts
@@ -1,14 +1,27 @@
 import { isAxiosError } from "axios"
 import api from "../lib/axios"
-import { ProjectFormData } from "../types"
+import { dashboardProjectSchema, ProjectFormData } from "../types"
 
 export async function createProject(formData: ProjectFormData) {
     try {
         const { data } = await api.post('/projects', formData)
-        console.log('_____>', data)
         return data
     } catch (error) {
         if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function getProjects() {
+    try {
+        const { data } = await api('/projects')
+        const response = dashboardProjectSchema.safeParse(data)
+        if(response.success) {
+            return response.data
+        }
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
         }
     }
