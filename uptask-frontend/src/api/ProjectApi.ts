c6a58ffc190dd47e76b620a94b01dd3b48f026b4
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-04-01 22:11:52 -0600

    Implementacion de funciones para obtener y actualizar proyectos desde la API

--- uptask-frontend/src/api/ProjectApi.ts
+++ uptask-frontend/src/api/ProjectApi.ts
@@ -1,6 +1,6 @@
 import { isAxiosError } from "axios"
 import api from "../lib/axios"
-import { dashboardProjectSchema, ProjectFormData } from "../types"
+import { dashboardProjectSchema, Project, ProjectFormData } from "../types"
 
 export async function createProject(formData: ProjectFormData) {
     try {
@@ -17,11 +17,41 @@ export async function getProjects() {
     try {
         const { data } = await api('/projects')
         const response = dashboardProjectSchema.safeParse(data)
-        if(response.success) {
+        if (response.success) {
             return response.data
         }
     } catch (error) {
-        if(isAxiosError(error) && error.response) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function getProjectById(id: Project['_id']) {
+    try {
+        const { data } = await api(`/projects/${id}`)
+        return data
+
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+type ProjectAPIType = {
+    formData: ProjectFormData
+    projectId: Project['_id']
+}
+export async function updateProject({ formData, projectId }: ProjectAPIType) {
+    try {
+        const { data } = await api.put(`/projects/${projectId}`, formData)
+
+
+        return data
+
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
         }
     }
