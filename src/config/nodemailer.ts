c1ac848ae35f0b52710df74a8848edfb4f73a393
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-04-14 20:37:05 -0600

    Se actualizan el .env para las credenciales de nodemialer

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -5,12 +5,12 @@ dotenv.config()
 
 const config = () => {
     return {
-        host: process.env.SMTP_HOST,
-        port: +process.env.SMTP_PORT,
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASS
-        }
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "2436e09803c9b7",
+        pass: "87e3c849ab492b"
+      }
     }
 }
 
