c53dfbd10a8b4875ed2f55ac7eacb42cde491f0c
AntonioValentinSayago <cesar_anto30@hotmail.com>
2025-04-15 17:12:40 -0600

    Configuracion de las rutas para mostrar del lado del cliente

--- uptask-frontend/src/router.tsx
+++ uptask-frontend/src/router.tsx
@@ -4,16 +4,31 @@ import DashboardView from "./views/DashboardView";
 import CreateProjectView from "./views/projects/CreateProjectView";
 import EditProjectView from "./views/projects/EditProjectView";
 import ProjectDetailsView from "./components/projects/ProjectDetailsView";
+import AuthLayout from "./layouts/AuthLayout";
+import LoginView from "./views/auth/LoginView";
+import RegisterView from "./views/auth/RegisterView";
+import ConfirmAccountView from "./views/auth/ConfirmAccountView";
+import RequestNewCodeView from "./views/auth/RequestNewCodeView";
+import ForgotPasswordView from "./views/auth/ForgotPasswordView";
+import NewPasswordView from "./views/auth/NewPasswordView";
 
 export default function Router() {
-    return(
+    return (
         <BrowserRouter>
             <Routes>
-                <Route element={<AppLayout/>}>
-                    <Route path='/' index element={<DashboardView />}/>
-                    <Route path='/projects/create' element={<CreateProjectView />}/>
+                <Route element={<AppLayout />}>
+                    <Route path='/' index element={<DashboardView />} />
+                    <Route path='/projects/create' element={<CreateProjectView />} />
                     <Route path='/projects/:projectId' element={<ProjectDetailsView />} />
                     <Route path='/projects/:projectId/edit' element={<EditProjectView />} />
+                </Route>
+                <Route element={<AuthLayout />}>
+                    <Route path='/auth/login' element={<LoginView />} />
+                    <Route path='/auth/register' element={<RegisterView />} />
+                    <Route path='/auth/confirm-account' element={<ConfirmAccountView />} />
+                    <Route path='/auth/request-code' element={<RequestNewCodeView />} />
+                    <Route path='/auth/forgot-password' element={<ForgotPasswordView/>} />
+                    <Route path='/auth/new-password' element={<NewPasswordView />} />
                 </Route>
             </Routes>
         </BrowserRouter>
